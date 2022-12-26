76dfbdef0f9e5e538449982ff6e6fe4f52b8f26a
Karol <karol660rata@gmail.com>
2022-12-26 16:52:33 +0100

    poprawka env

--- server.js
+++ server.js
@@ -106,6 +106,6 @@ io.on('connection', (socket) => {
 });
 
 
-server.listen(3001, () => {
-    console.log('server dziala na 3001');
+server.listen(process.env.PORT || 3001, () => {
+    console.log(`server dziala na `);
 });
